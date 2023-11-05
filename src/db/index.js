9d103ca6fef19a573eb023fd31f0848277ebaf3e
biplavsitaula <biplav0007@gmail.com>
2023-11-05 10:52:19 +0545

    db connection

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MongoDB connextion error", error);
+        console.log("MongoDB connetion error", error);
         process.exit(1)
     }
 }
